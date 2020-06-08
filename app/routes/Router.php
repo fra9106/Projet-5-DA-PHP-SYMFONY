74dcbbce6a6c275af18b3740d06c142b04f840e4
fra9106 <55781079+fra9106@users.noreply.github.com>
2020-06-08 12:43:31 +0200

    display form and logic login/logout

--- app/routes/Router.php
+++ app/routes/Router.php
@@ -11,107 +11,112 @@ class Router
     {
         try
         {
-            if (isset($_GET['action']))
-            {
+            if (isset($_GET['action'])){
                 
-                if ($_GET['action'] == 'homePage')
-                {
+                if ($_GET['action'] == 'homePage'){
                     $display = new ControllerHome();
                     $contact = $display->homePage();
                     
                 }
                 
-                if ($_GET['action'] == 'displFormulContact')
-                {
+                if ($_GET['action'] == 'displFormulContact'){
                     $display = new ControllerUser();
                     $contact = $display->displFormulContact();
-                    
+                }
+
+                if ($_GET['action'] == 'displConnexion'){
+                $display = new ControllerUser();
+                $contact = $display->displConnexion();
+                }
+
+                if ($_GET['action'] == 'login'){
+                    if (isset($_POST['login']) and isset($_POST['mail']) and isset($_POST['mdp'])){
+                        $mail = htmlspecialchars($_POST['mail']);
+
+                        if (!empty(trim($_POST['mail'])) and !empty(trim($_POST['mdp']))){
+                        $connex = new ControllerUser();
+                        $newConnexion = $connex->login($_POST['mail'], $_POST['mdp']);
+                        }
+                        else{
+                        throw new Exception('Oups...Tous les champs doivent être complétés !');
+                        }
+                    }
+                }
+
+                if ($_GET['action'] == 'logout'){
+                $lout = new ControllerUser();
+                $logOut = $lout->logout();
+
                 }
                 
-                if ($_GET['action'] == 'addMember')
-                {
+                if ($_GET['action'] == 'addMember'){
                     if (isset($_POST['addMember']) and isset($_POST['pseudo']) and isset($_POST['mail']) and isset($_POST['mdp']))
                     {
                         $pseudo = htmlspecialchars($_POST['pseudo']);
                         $mail = htmlspecialchars($_POST['mail']);
-                        if (!empty($_POST['pseudo']) and !empty($_POST['mail']) and !empty($_POST['mdp']))
-                        {
+                        if (!empty($_POST['pseudo']) and !empty($_POST['mail']) and !empty($_POST['mdp'])){
                             $pseudolength = strlen($pseudo);
                             if ($pseudolength > 2)
                             {
-                                if (filter_var($mail, FILTER_VALIDATE_EMAIL))
-                                {
+                                if (filter_var($mail, FILTER_VALIDATE_EMAIL)){
                                     $inscription = new ControllerUser();
                                     $contact = $inscription->addMember($_POST['pseudo'], $_POST['mail'], $_POST['mdp'], 'default.jpg');
                                     return $contact;
                                 }
-                                else
-                                {
+                                else{
                                     throw new Exception('Adresse mail non valide !');
                                 }
                             }
-                            else
-                            {
+                            else{
                                 throw new Exception('Votre pseudo doit contenir plus de deux caractères !');
                             }
                         }
-                        else
-                        {
+                        else{
                             throw new Exception('Tous les champs doivent être complétés');
                         }
                     }
                 }
                 
-                if ($_GET['action'] == 'listArticles')
-                {
+                if ($_GET['action'] == 'listArticles'){
                     $listarticles = new ControllerArticles();
                     $artic = $listarticles->listArticle();
                     //var_dump($artic); die;
-                    
-                }
+                    }
                 
-                if ($_GET['action'] == 'writeArticleDisplay')
-                {
+                if ($_GET['action'] == 'writeArticleDisplay'){
                     
                     $controlleradmin = new ControllerArticles();
                     $adminconnect = $controlleradmin->formArticle();
-                    
-                }
+                    }
                 
-                if ($_GET['action'] == 'articleWriting')
-                {
+                if ($_GET['action'] == 'articleWriting'){
                     
-                    if (isset($_POST['send_article']) and isset($_POST['id_category']) /*and isset($_SESSION['id_user'])*/ and isset($_POST['mini_content']) and isset($_POST['title']) and isset($_POST['content']))
-                    {
+                    if (isset($_POST['send_article']) and isset($_POST['id_category']) and isset($_SESSION['id_user']) and isset($_POST['mini_content']) and isset($_POST['title']) and isset($_POST['content'])){
                         
                         $idCategory = ($_POST['id_category']);
-                        //$idUser = ($_SESSION['id_user']);
+                        $idUser = ($_SESSION['id_user']);
                         $miniContent = ($_POST['mini_content']);
                         $title = ($_POST['title']);
                         $content = ($_POST['content']);
                         
-                        if (!empty(trim($_POST['mini_content'])) and !empty(trim($_POST['title'])) and !empty(trim($_POST['content'])))
-                        {
+                        if (!empty(trim($_POST['mini_content'])) and !empty(trim($_POST['title'])) and !empty(trim($_POST['content']))){
                             $articleWrite = new ControllerArticles();
                             $display = $articleWrite->articleWriting($idCategory, $idUser, $miniContent, $title, $content);
                             
                         }
-                        else
-                        {
+                        else{
                             throw new Exception('Vous n\'avez pas saisi d\'article !');
                         }
                     }
                 }
                 
             }
-            else
-            {
+            else{
                 $vue = new ControllerHome();
                 $accueil = $vue->homePage();
             }
         }
-        catch(Exception $e)
-        {
+        catch(Exception $e){
             $errorMessage = $e->getMessage();
             //require ('views/frontend/errorView.php');
             echo "error";
